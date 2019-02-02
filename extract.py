5f457899e49838401b76554159262ac507843e51
Mars <mfogels@gmail.com>
2019-02-01 19:09:36 -0500

    Upgrade data extraction script to use ActiveData and Buildhub.

--- extract.py
+++ extract.py
@@ -5,13 +5,18 @@
 Extract data about Mozilla's engineering workflow from various tool systems.
 """
 
+import logging
+import sys
+from itertools import islice
+
 import pandas as pd
 import numpy as np
-
 import requests
-
 import tqdm
 from mozautomation import commitparser
+from kinto_http import Client as KintoClient
+
+log = logging.getLogger()
 
 # Progress bars!
 tqdm.tqdm.pandas()
@@ -22,6 +27,33 @@ LOCAL_HG_SERVER = "http://localhost:9999"
 
 # Some searches need to hit hg.mozilla.org directly because hg.m.o has custom plugins.
 HGMO_URL = "https://hg.mozilla.org/"
+
+# Buildhub service, which holds all Firefox build artifact data.
+BUILDHUB_URL = "https://buildhub.prod.mozaws.net/v1"
+
+# We use the ActiveData warehouse service to give us bug records.  Very snappy.
+ACTIVEDATA_URL = "https://activedata-public.devsvcprod.mozaws.net/query"
+
+
+def get_nightly_builds(from_datestr="2018-10", to_datestr="2018-11"):
+    """Return nightly build data for all builds."""
+    client = KintoClient(server_url=BUILDHUB_URL)
+    records = client.get_records(
+        **{
+            "target.platform": "linux-x86_64",
+            "target.channel": "nightly",
+            "source.product": "firefox",
+            "target.locale": "en-US",
+            "gt_download.date": from_datestr,
+            "lt_download.date": to_datestr,
+            "_sort": "-download.date",
+            "_fields": "build.id,source.revision,download.date",
+            # "_limit": 10,
+        },
+        bucket="build-hub",
+        collection="releases",
+    )
+    return records
 
 
 def get_pushes_in_range(from_changeset, to_changeset):
@@ -52,25 +84,85 @@ def parse_bug_id(desc):
     """Parse a Firefox VCS commit message and return the bug ID, if possible."""
     ids = commitparser.parse_bugs(desc)
     if ids:
-        # Leave it as a string.  Multiple bug id#s doesn't happen in practice.
-        return str(ids[0])
+        # Multiple bug id#s doesn't happen in practice.
+        return int(ids[0])
     else:
         return np.NaN
 
 
-def fetch_bug_creation_time(bug_id):
-    """Fetch a bug's creation time from BMO."""
-    r = requests.get(f"https://bugzilla.mozilla.org/rest/bug/{bug_id}")
+def fetch_bug_creation_times(bug_ids):
+    """Return a DataFrame of bug ids and their creation times."""
+    # See https://github.com/mozilla/ActiveData/blob/dev/docs/jx_tutorial.md for the
+    # syntax.
+    query = {
+        "from": "public_bugs",
+        "select": {"aggregate": "min", "value": "created_ts"},
+        "groupby": "bug_id",
+        "where": {"in": {"bug_id": list(bug_ids)}},
+        "limit": len(bug_ids),
+        "format": "table",
+    }
+    # Note that the bug list we ask for is allowed to be large.  The upper limit on
+    # ActiveData responses is 50K records!
+    r = requests.post(ACTIVEDATA_URL, json=query)
     if not r.ok:
-        return np.NaN
-    return pd.to_datetime(r.json()["bugs"][0]["creation_time"])
+        raise RetrievalError(r.content)
+
+    df = pd.DataFrame(r.json()["data"], columns=["bug", "creation_ts"])
+
+    # Convert to Pandas datetimes
+    df["bug_creation_time"] = pd.to_datetime(df["creation_ts"], unit="ms")
+    df = df.drop(columns="creation_ts")
+
+    return df
 
 
 def main():
-    nightly_build_id = "20190122094123"
-    nightly_publish_time = "2019-01-22T12:16:52Z"
-    nightly_changeset_id = "f0c23db0d035dbe81e23eb4d619e493e38582d24"
-    prev_nightly_changeset_id = "44369796f148630ff496be99f77a5eeea41c7d23"
+    logging.basicConfig(stream=sys.stdout)
+
+    df = pd.DataFrame()
+
+    from_date = "2017-10"
+    to_date = "2017-11"
+
+    builds = get_nightly_builds(from_date, to_date)
+    build_count = len(builds)
+
+    print(f"Found {build_count} builds")
+    print("Extracting build data from sources")
+
+    for target_build, prev_build in tqdm.tqdm(
+        zip(islice(builds, 0, None), islice(builds, 1, None)),
+        desc="builds",
+        total=build_count,
+    ):
+        build_id = target_build["build"]["id"]
+        build_rev = target_build["source"]["revision"]
+        prev_rev = prev_build["source"]["revision"]
+
+        log.debug("Processing build", build_id)
+        try:
+            cd = get_data_for_commit_range(build_rev, prev_rev)
+        except RetrievalError as e:
+            log.warning("Skipping build %s" % target_build)
+            log.warning("Reason: %s" % e)
+            continue
+
+        cd["nightly_build_id"] = build_id
+        cd["nightly_publish_time"] = pd.to_datetime(target_build["download"]["date"])
+        df = pd.concat([df, cd])
+
+    print()
+    print(f"Processed {df.shape[0]} records with {df.shape[1]} columns")
+    print(f"Columns: {df.columns.to_list()}")
+
+    outfile_name = "output.parq"
+    df.to_parquet(outfile_name)
+    print(f"Wrote {outfile_name}")
+
+
+def get_data_for_commit_range(nightly_changeset_id, prev_nightly_changeset_id):
+    """Return a DataFrame with workflow data between commits X and Y."""
 
     pushes = get_pushes_in_range(prev_nightly_changeset_id, nightly_changeset_id)
 
@@ -85,18 +177,21 @@ def main():
     # Fix the name of the first column that json_normalize() gave us.
     df = df.rename(columns={0: "changeset"})
 
+    if df.empty:
+        raise RetrievalError(
+            f"No push data could be retrieved for "
+            f"{nightly_changeset_id} to {prev_nightly_changeset_id}"
+        )
+
     # Convert push dates from Unix time to datetime
     df["changeset_pushtime"] = df["changeset_data_date"].apply(
         lambda t: pd.to_datetime(t, unit="s")
     )
     df = df.drop(columns="changeset_data_date")
 
-    # Add our nightly build metadata
-    df["nightly_build_id"] = nightly_build_id
-    df["nightly_publish_time"] = pd.to_datetime(nightly_publish_time)
-
     # Add changeset summaries
-    print("Getting changeset summaries")
+    log.debug("Getting changeset summaries")
+    tqdm.tqdm.pandas(desc="commit summaries")
     df["changeset_desc"] = df["changeset"].progress_apply(fetch_rev_summary)
 
     # Re-order the columns so 'changeset_' values are together
@@ -108,28 +203,30 @@ def main():
     # Drop all commits with no bug number
     commits_with_bugs = df[df["bug"].notna()]
     dropped = len(df) - len(commits_with_bugs)
-    print(f"Dropped {dropped} commits with no associated bug number")
+    log.debug(f"Dropped {dropped} commits with no associated bug number")
 
     df = commits_with_bugs
 
+    if df.empty:
+        raise RetrievalError("No public bugs could be found for this build")
+
     # Add bug creation times
-    print("Fetching bug creation times")
-    df["bug_creation_time"] = df["bug"].progress_apply(fetch_bug_creation_time)
+    log.debug("Fetching bug creation times")
+    creation_times = fetch_bug_creation_times(df["bug"])
+    df = pd.merge(df, creation_times, on="bug")
 
     # Not all bugs are publicly visible.  We won't have data for these bugs.
     public_bugs = df[df["bug_creation_time"].notna()]
     dropped = len(df) - len(public_bugs)
-    print(f"Dropped {dropped} bugs that we couldn't access the data for")
+    log.debug(f"Dropped {dropped} bugs that we couldn't access the data for")
 
     df = public_bugs
 
-    print()
-    print(f"Processed {df.shape[0]} records with {df.shape[1]} columns")
-    print(f"Columns: {df.columns.to_list()}")
+    return df
 
-    outfile_name = "output.parq"
-    df.to_parquet(outfile_name)
-    print(f"Wrote {outfile_name}")
+
+class RetrievalError(Exception):
+    pass
 
 
 if __name__ == "__main__":
